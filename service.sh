84b30842853d34f9349cd8b06ddb28f3497dd3bb
Tkocean <109120776+Tkocean@users.noreply.github.com>
2023-07-24 12:05:05 +0800

    Add files via upload

--- service.sh
+++ service.sh
@@ -5,7 +5,7 @@
  done
 
  echo "PowerManagerService.noSuspend" > /sys/power/wake_lock
- chown 1000:1000 /data/adb/modules/Aurora/config/Aurora.ini && chmod 004 /data/adb/modules/Aurora/config/Aurora.ini && chown 0:0 /system/bin/Aurora && chmod 6110 /system/bin/Aurora && /system/bin/Aurora -n
+ chown 1000:1000 /data/adb/modules/Aurora/config/Aurora.ini && chmod 004 /data/adb/modules/Aurora/config/Aurora.ini && chown 0:0 /system/bin/Aurora && chmod 7110 /system/bin/Aurora && /system/bin/Aurora -n
 )&
 
 exit 0
--- uninstall.sh
+++ uninstall.sh
@@ -1,7 +1,10 @@
-cp -r /cache/backup /storage/emulated/0/Android/Aurora
+#!/system/bin/sh
+cp -r /data/cache/backup /storage/emulated/0/Android/Aurora
 
 rm -rf /data/adb/Aurora
-rm -rf /cache/backup
+rm -rf /data/cache/backup/*
 rm -rf /data/system/Dashboard
-rm -rf /data/cache/temp
-rm -rf /data/system/package_cache
+rm -rf /data/cache/temp/*
+rm -rf /data/system/package_cache/*
+
+echo "  Aurora uninstalled from your device." > /cache/magisk.log
