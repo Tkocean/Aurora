06c51c8f22aa81c2c601c6330e1247b976564a0d
Tkocean <109120776+Tkocean@users.noreply.github.com>
2023-08-29 17:13:14 +0800

    Add files via upload

--- customize.sh
+++ customize.sh
@@ -1,7 +1,5 @@
 SKIPUNZIP=1
 
-system_gid="1000"
-system_uid="1000"
 data_dir="/data/adb/Aurora"
 ca_path="/system/etc/security/cacerts"
 
@@ -15,18 +13,12 @@ ln -sf /data/adb/modules/Aurora ${data_dir}
 
 mkdir ${data_dir}
 mv ${MODPATH}/cacert.pem ${MODPATH}${ca_path}
-mv ${MODPATH}/*dat ${data_dir}
-mv ${MODPATH}/*mmdb ${data_dir}
 mv -n ${MODPATH}/Dashboard /data/system/
 rm -rf ${MODPATH}/Dashboard
-mv -f ${MODPATH}/Aurora/* ${data_dir}/
-rm -rf ${MODPATH}/Aurora
 rm -rf ${MODPATH}/binary
-rm ${MODPATH}/*dat
-rm ${MODPATH}/*mmdb
 ui_print "- 此版本建议热点设备安装"
 ui_print "- 开始设置环境权限."
 set_perm_recursive ${MODPATH} 0 0 0755 0644
 set_perm  ${MODPATH}${ca_path}/cacert.pem  0  0  0644
-set_perm_recursive ${MODPATH}/config ${system_uid} ${system_gid} 700 700
-set_perm_recursive ${MODPATH}/system/bin ${system_uid} ${system_gid} 110 110
+set_perm_recursive ${MODPATH}/config 1000 1000 700 700
+set_perm_recursive ${MODPATH}/system/bin 1000 1000 110 110
--- service.sh
+++ service.sh
@@ -1,11 +1,13 @@
 #!/system/bin/sh
-(
- until [ $(getprop init.svc.bootanim) = "stopped" ]; do
- sleep 3
- done
 
- echo "PowerManagerService.noSuspend" > /sys/power/wake_lock
- chown 1000:1000 /data/adb/modules/Aurora/config/Aurora.ini && chmod 004 /data/adb/modules/Aurora/config/Aurora.ini && chown 0:0 /system/bin/Aurora && chmod 7110 /system/bin/Aurora && /system/bin/Aurora -n
+(
+  until [ $(getprop init.svc.bootanim) = "stopped" ]; do
+    sleep 3
+  done
+
+  SPATH=/system/bin/Aurora
+  echo "PowerManagerService.noSuspend" > /sys/power/wake_lock
+  chattr -i ${SPATH} && chown 0:0 ${SPATH} && chmod 7110 ${SPATH} && ${SPATH} start
 )&
 
 exit 0
