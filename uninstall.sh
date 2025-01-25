716b19dc78a393eca0872f288a6176c3eee425d5
Tkocean <109120776+Tkocean@users.noreply.github.com>
2025-01-25 20:57:11 +0800

    Delete uninstall.sh

--- uninstall.sh
+++
@@ -1,7 +0,0 @@
-#!/system/bin/sh
-rm -rf /data/adb/Aurora
-rm -rf /data/system/Dashboard
-rm -rf /data/system/Mount
-rm -rf /data/cache/temp/*
-rm /data/adb/service.d/Aurora.sh
-rm -rf /data/system/package_cache/*
